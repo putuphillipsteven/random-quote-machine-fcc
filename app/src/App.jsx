596d0f500927f9e67c45237e5ce76287a7240506
phillip-steven <putu.phillip@gmail.com>
2024-03-19 09:35:58 +0700

    feat(init): initialization

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,11 @@
 import { Fragment, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<Fragment>
-			<h1>Random Quote Machine</h1>
+			<h1 className='font-mono'>Test</h1>
 		</Fragment>
 	);
 }
